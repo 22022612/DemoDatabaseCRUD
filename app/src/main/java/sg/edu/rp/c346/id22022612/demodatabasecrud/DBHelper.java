99d75ea687559a440ca89e3048bc55734cfa4708
clarence ang <22022612@myrp.edu.sg>
2023-07-13 10:15:26 +0800

    Section B

--- app/src/main/java/sg/edu/rp/c346/id22022612/demodatabasecrud/DBHelper.java
+++ app/src/main/java/sg/edu/rp/c346/id22022612/demodatabasecrud/DBHelper.java
@@ -66,4 +66,14 @@ public class DBHelper extends SQLiteOpenHelper {
         db.close();
         return notes;
     }
+    public long insertNote(String noteContent) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOTE_CONTENT, noteContent);
+        long result = db.insert(TABLE_NOTE, null, values);
+        db.close();
+        Log.d("SQL Insert","ID:"+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
 }
